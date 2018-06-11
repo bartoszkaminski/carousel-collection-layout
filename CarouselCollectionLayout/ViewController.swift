02911c0213d610183ac48e8e7c57496e4e083a51
Bartosz Kamiński <bartosz@kaminski.me>
2018-06-11 20:41:31 +0200

    Initial Xcode project

---
+++ CarouselCollectionLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarouselCollectionLayout
+//
+//  Created by Bartosz Kamiński on 11/06/2018.
+//  Copyright © 2018 Bartosz Kamiński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
